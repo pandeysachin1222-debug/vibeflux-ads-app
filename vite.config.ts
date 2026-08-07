14bbf31eb5ce882dac0edc1bba89754a73f7c6f5
pandeysachin1222-debug <pandey.sachin1222@gmail.com>
2026-08-07 10:01:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
   vite: {
-    plugins: [
-      tsconfigPaths({
-        ignoreConfigErrors: true,
-      }),
-    ],
     resolve: {
       alias: {
         "@": "/src",
